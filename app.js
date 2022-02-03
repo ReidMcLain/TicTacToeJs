4ee72361d510ab0d10bb9be9914ebbca5436db17
ReidMcLain <reidmm@gmail.com>
2022-02-02 19:07:16 -0600

    added some styles

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ let cells = document.querySelectorAll('.row > div');
 
 console.log(cells);
 
-let winner = []
+let gameWon = false;
 
 for (let i = 0; i < cells.length; i++) {
     cells[i].addEventListener('click', cellClicked);
@@ -43,44 +43,51 @@ let winningCombo = [
     [cells[2], cells[4], cells[6]]
 ]
 
-function checkWinner() {
-    for (let i = 0; i < winningCombo.length; i++) {
-        let xCount = 0;
-        let oCount = 0;
-        for (let j = 0; j < winningCombo[i].length; j++) {
-            if (winningCombo[i][j].innerHTML == 'X') {
-                xCount++
-                console.log(xCount)
-            }
-            else if (winningCombo[i][j].innerHTML == 'O') {
-                oCount++
-                console.log(oCount)
-            }
-        }
-        if (xCount == 3) {
-            setTimeout(function () {
-                alert('X Wins!')
-                location.reload();
-            }, 0);
-        }
-        else if (oCount == 3) {
-            setTimeout(function () {
-                alert('O Wins!')
-                location.reload();
-            }, 0);
-
-        }
-    }
-}
-
 function checkDraw() {
-    if (turnCounter == 9) {
+    if (turnCounter == 9 && gameWon == false) {
         setTimeout(function () {
             alert('It is a Draw!')
             location.reload();
-        }, 0);
+        }, 100);
     }
 }
+
+function checkWinner() {
+    if (!gameWon) {
+        for (let i = 0; i < winningCombo.length; i++) {
+            let xCount = 0;
+            let oCount = 0;
+            for (let j = 0; j < winningCombo[i].length; j++) {
+                if (winningCombo[i][j].innerHTML == 'X') {
+                    xCount++
+                    console.log(xCount)
+                }
+                else if (winningCombo[i][j].innerHTML == 'O') {
+                    oCount++
+                    console.log(oCount)
+                }
+            }
+            if (xCount == 3) {
+                gameWon = true;
+
+                setTimeout(function () {
+                    alert('X Wins!')
+                    location.reload();
+                }, 100);
+            }
+            else if (oCount == 3) {
+                gameWon = true;
+
+                setTimeout(function () {
+                    alert('O Wins!')
+                    location.reload();
+                }, 100);
+
+            }
+        }
+    }
+}
+
 
 const reset = () => {
     spaces.forEach((spaces, i) => {
@@ -94,4 +101,4 @@ const reset = () => {
     turnCounter = 0;
 }
 
-resetButton.addEventListener('click', reset)
+resetButton.addEventListener('click', reset) 
